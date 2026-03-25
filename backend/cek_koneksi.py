6edf4866a4b1ec78c8685ece2698a0b0821f4682
DIAN PRANSISKO HARAHAP <30459039+dianpransisko@users.noreply.github.com>
2026-03-25 20:19:29 +0700

    Update cek_koneksi.py

--- backend/cek_koneksi.py
+++ backend/cek_koneksi.py
@@ -1,8 +1,8 @@
-from app.db.supabase_client import supabase
+from #
 
 try:
     # Mencoba mengambil data (walaupun tabel kosong)
-    res = supabase.table("scraped_data").select("id").limit(1).execute()
+    res = #
     print("✅ MANTAP! Koneksi ke Supabase Berhasil.")
 except Exception as e:
     print(f"❌ Masih Error: {e}")
